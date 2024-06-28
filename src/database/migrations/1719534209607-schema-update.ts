77eacc431549d4a320b0f1cc9ec2fbf6aaa08d72
Éder Pires <ederpbj@gmail.com>
2024-06-27 22:41:06 -0300

    novo migrate gerado

---
+++ src/database/migrations/1719534209607-schema-update.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class SchemaUpdate1719534209607 implements MigrationInterface {
+    name = 'SchemaUpdate1719534209607'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "genres" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, CONSTRAINT "PK_80ecd718f0f00dde5d77a9be842" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "games" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "description" character varying NOT NULL, "dateRelease" date NOT NULL, "rating" double precision NOT NULL, "site" character varying NOT NULL, "urlImage" character varying NOT NULL, "genreId" uuid, CONSTRAINT "PK_c9b16b62917b5595af982d66337" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`ALTER TABLE "games" ADD CONSTRAINT "FK_774df247eee80a13840bcd90f7b" FOREIGN KEY ("genreId") REFERENCES "genres"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "games" DROP CONSTRAINT "FK_774df247eee80a13840bcd90f7b"`);
+        await queryRunner.query(`DROP TABLE "users"`);
+        await queryRunner.query(`DROP TABLE "games"`);
+        await queryRunner.query(`DROP TABLE "genres"`);
+    }
+
+}
